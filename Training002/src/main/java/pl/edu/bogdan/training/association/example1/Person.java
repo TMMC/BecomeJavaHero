ee5df0f8ec5481b6f5b76ee64a28e21179bfa2d2
Damian <damian_bober@interia.pl>
2017-10-10 09:31:30 +0200

    zmiana na distance 1

--- Training002/src/main/java/pl/edu/bogdan/training/association/example1/Person.java
+++ Training002/src/main/java/pl/edu/bogdan/training/association/example1/Person.java
@@ -2,13 +2,14 @@ package pl.edu.bogdan.training.association.example1;
 
 public class Person {
 	private String name;
+	public int pole;
 
 	public Person(String name) {
 		this.name = name;
 	}
 	
-	public void travel(Car car, int distance) {
-		car.go(distance);
+	public void travel(Car car, int distance1) {
+		car.go(distance1);
 	}
 	
 	public void setName(String name) {
