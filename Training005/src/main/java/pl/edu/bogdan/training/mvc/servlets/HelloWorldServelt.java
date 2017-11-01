5196cd3589c250f5c262aa6322afbd84043cc422
Paweł Bogdan <pawel.bogdan@gmail.com>
2017-11-01 09:50:19 +0100

    First servlet

---
+++ Training005/src/main/java/pl/edu/bogdan/training/mvc/servlets/HelloWorldServelt.java
@@ -0,0 +1,21 @@
+package pl.edu.bogdan.training.mvc.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldServelt extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("    <body>");
+		out.println("        Hello from Servlet");
+		out.println("    </body>");
+		out.println("</html>");
+		out.flush();
+		out.close();
+	}
+}
